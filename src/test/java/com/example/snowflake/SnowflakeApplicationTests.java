daf7d711f08335e81fedceb8a6ce22cdcca27aba
zhengweilin <568633995@qq.com>
2019-09-17 17:19:49 +0800

        雪花算法不依赖于数据库本身，是分布式id生成算法中比较经典的一种。整个ID的构成大概分为这么几个部分，时间戳差值，机器编码，进程编码，序列号。java的long是64位的从左向右依次介绍是：时间戳差值，在我们这里占了42位；机器编码5位；进程编码5位；序列号12位。所有的拼接用位运算拼接起来，于是就基本做到了每个进程中不会重复了。

---
+++ src/test/java/com/example/snowflake/SnowflakeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.snowflake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnowflakeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        long id = SnowFlake.nextId();
+        System.out.println(id);
+    }
+
+}
